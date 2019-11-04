0149df60fbf7d1bd9c60eabb1cfd8b19e01d5fe5
Douglas Sousa <douglascamilo@gmail.com>
2019-11-04 01:37:11 -0300

    [Aula 03] - $http e promises

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <h1>{{ title }}</h1>
     <ul>
       <li v-for="photo of photos">
-        <img :src="photo.imgUrl" :alt="photo.title">
+        <img :src="photo.url" :alt="photo.titulo">
       </li>
     </ul>
   </div>
@@ -11,18 +11,25 @@
 
 <script>
 export default {
+  created() {
+    this.$http.get('http://localhost:3000/v1/fotoss')
+      .then(response => response.json())
+      .then(photos => this.photos = photos)
+      .catch(error => console.log(error.data));
+  },
+
   data() {
     return {
       title: 'Alurapic - Vue.js',
       photos: [
-        {
-          imgUrl: 'https://cdn.pixabay.com/photo/2017/10/04/18/11/road-2817102_960_720.jpg',
-          title: 'Horizon'
-        },
-        {
-          imgUrl: 'https://cdn.pixabay.com/photo/2019/10/19/12/21/hot-air-balloons-4561267_960_720.jpg',
-          title: 'Ballon'
-        }
+        // {
+        //   url: 'https://cdn.pixabay.com/photo/2017/10/04/18/11/road-2817102_960_720.jpg',
+        //   titulo: 'Horizon'
+        // },
+        // {
+        //   url: 'https://cdn.pixabay.com/photo/2019/10/19/12/21/hot-air-balloons-4561267_960_720.jpg',
+        //   titulo: 'Ballon'
+        // }
       ]
     }
   }
